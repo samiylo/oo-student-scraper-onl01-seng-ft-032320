6821218b654bebc11e051289806f0e0cecf36aa4
Samiylo Kryshu <skryshu@gmail.com>
2020-04-11 16:39:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,7 +13,7 @@ class Scraper
       hash = {
         :name => student.css("h4.student-name").text,
         :location => student.css("p.student-location").text,
-        :profile_url => "http://students.learn.co/" + student.css("a").attribute("href")
+        :profile_url => student.css("a").attribute("href")
       }
       students << hash 
     end
