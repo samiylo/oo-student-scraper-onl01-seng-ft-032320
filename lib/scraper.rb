86d625fb834f500c1fd89ca60b21ba1fe332661f
Samiylo Kryshu <skryshu@gmail.com>
2020-04-11 17:01:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -39,7 +39,9 @@ class Scraper
         student[:blog] = link
       end
     end
-      
+      student[:profile_quote] = page.css(".profile-quote").text
+      student[:bio] = page.css("div.description-holder p").text
+      student
   end
 
 end
